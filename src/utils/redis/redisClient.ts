0b418c4e7c2ef8f7d7030fe34e6c2c64f9e145dd
Furkan Abay <furkan_abay@hotmail.com>
2025-04-24 00:09:19 +0200

    feat: migrated to self-hosted redis

--- src/utils/redis/redisClient.ts
+++ src/utils/redis/redisClient.ts
@@ -2,10 +2,5 @@ import { createClient, RedisClientType } from 'redis';
 import 'dotenv/config';
 
 export const redisClient: RedisClientType = createClient({
-  username: process.env.REDIS_USERNAME,
-  password: process.env.REDIS_PASSWORD,
-  socket: {
-    host: process.env.REDIS_HOST,
-    port: 15659
-  }
+  url: process.env.REDIS_URL
 });
